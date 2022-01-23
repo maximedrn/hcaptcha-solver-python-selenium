25a880f09a0de619d68cbb245aabf7b45bb845a5
Maxime Dréan <91475935+maximedrn@users.noreply.github.com>
2022-01-23 12:02:43 +0100

    Update main.py: lobatim:patch-1
    https://github.com/maximedrn/hcaptcha-solver-python-selenium/pull/5

--- main.py
+++ main.py
@@ -42,7 +42,8 @@ class hCaptcha:
         """Start webdriver and return state of it."""
         options = webdriver.ChromeOptions()  # Configure options for Chrome.
         options.add_extension(self.extension_path)  # Add extension.
-        options.add_experimental_option('prefs', {'intl.accept_languages': 'en,en_US'})  # Set webdriver language to English.
+        options.add_experimental_option(  # Set webdriver language to English.
+            'prefs', {'intl.accept_languages': 'en,en_US'})
         # options.add_argument("headless")  # Headless ChromeDriver.
         options.add_argument('log-level=3')  # No logs is printed.
         options.add_argument('--mute-audio')  # Audio is muted.
