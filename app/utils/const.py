c4bef388da53290d7a37850e2c929221b826ff63
Maxime <91475935+maximedrn@users.noreply.github.com>
2022-07-19 12:23:21 +0200

    Update const.py

--- app/utils/const.py
+++ app/utils/const.py
@@ -14,7 +14,7 @@ Any distribution, modification or commercial use is strictly prohibited.
 
 
 # URL of the Greasy Fork userscript used in Tampermonkey.
-SCRIPT_URL = 'https://greasyfork.org/fr/scripts/445478-hcaptcha-solver'
+SCRIPT_URL = 'https://greasyfork.org/fr/scripts/448109-hcaptcha-solver'
 
 # URL of the demonstration website.
 DEMONSTRATION_URL = 'https://maximedrn.github.io/hcaptcha-solver-python-selenium/'
